7654b890adcb79f5df4a1a694d059339e98bda99
shakedshoshan <shakedshoshan8@gmail.com>
2024-07-07 13:03:49 +0300

    last update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import Image from "next/image";
 import { useEffect, useState } from "react";
-require("dotenv").config();
+
 // Array of currency codes
 const currencyCodes = [
   null,"AED", "AFN", "ALL", "AMD", "ANG", "AOA", "ARS", "AUD", "AWG", "AZN",
@@ -35,7 +35,7 @@ export default function Home() {
   }
 
   const getExchangeRate = async () =>{
-    const API_URL = `https://v6.exchangerate-api.com/v6/29f0d35ec8320c1eb15122f8/pair/${fromCurrency}/${toCurrency}`;
+    const API_URL = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/${fromCurrency}/${toCurrency}`;
     try{
       const response = await fetch(API_URL);
       if(!response.ok) throw new Error("Somthing not good!");
@@ -81,10 +81,10 @@ export default function Home() {
           </select>
         </div>
 
-        <div className="swap-icon" onClick={handleSwapCurrencies}>
+        <div className="scale-125 bg-[#5250d1] rounded-full p-3 hover:opacity-85 hover:scale-150 " onClick={handleSwapCurrencies}>
                     <svg width="16" viewBox="0 0 20 19" xmlns="http://www.w3.org/2000/svg">
                         <path d="M19.13 11.66H.22a.22.22 0 0 0-.22.22v1.62a.22.22 0 0 0 .22.22h16.45l-3.92 4.94a.22.22 0 0 0 .17.35h1.97c.13 0 .25-.06.33-.16l4.59-5.78a.9.9 0 0 0-.7-1.43zM19.78 5.29H3.34L7.26.35A.22.22 0 0 0 7.09 0H5.12a.22.22 0 0 0-.34.16L.19 5.94a.9.9 0 0 0 .68 1.4H19.78a.22.22 0 0 0 .22-.22V5.51a.22.22 0 0 0-.22-.22z"
-                            fill="#000" />
+                            fill="#FFF" />
                     </svg>
                 </div>
 
